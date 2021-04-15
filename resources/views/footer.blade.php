75191e8efefa308da68ee29da8ebcf7bfbbc2f3b
unknown <KETANGOLE@gmail.com>
2021-04-15 15:17:06 +0530

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,16 @@
+
+<div class="container">
+<div class="card text-center">
+  <div class="card-header">
+    Footer
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Welcome To Footer</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  
+  </div>
+  <div class="card-footer text-muted">
+  Footer End
+  </div>
+</div>
+</div>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,53 @@
+@extends('master')
+@section('content')
+
+<div class="container">
+<div class="text-center">
+<h1>Welcome To Login Page</h1>
+
+
+<main class="login-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Register</div>
+                    <div class="card-body">
+                        <form action="" method="">
+                            <div class="form-group row">
+                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                <div class="col-md-6">
+                                    <input type="text" id="email_address" class="form-control" name="email-address" required autofocus>
+                                </div>
+                            </div>
+                            <br>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                                <div class="col-md-6">
+                                    <input type="password" id="password" class="form-control" name="password" required>
+                                </div>
+                            </div>
+
+                     <br>
+
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                                
+                            </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</main>
+
+</div>
+</div>
+@endsection
+
