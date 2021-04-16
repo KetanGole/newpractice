22eb2bb278d3fd21de546dd34fd833f8fcd5f049
unknown <KETANGOLE@gmail.com>
2021-04-16 12:22:26 +0530

    login pages

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,11 +13,12 @@
                 <div class="card">
                     <div class="card-header">Register</div>
                     <div class="card-body">
-                        <form action="" method="">
+                        <form action="login" method="POST">
                             <div class="form-group row">
+                                @csrf
                                 <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
                                 <div class="col-md-6">
-                                    <input type="text" id="email_address" class="form-control" name="email-address" required autofocus>
+                                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
                                 </div>
                             </div>
                             <br>
@@ -33,7 +34,7 @@
 
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Login
                                 </button>
                                 
                             </div>
